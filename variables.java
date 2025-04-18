ef336ef04f706b201e9519677c52e6b526c738e0
Nkj-718 <jhanitin16@gmail.com>
2025-04-18 20:29:17 +0530

    Variables

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String[] args) {
+        int age=20;
+        double percent=73.8;
+        char grade='C';
+        boolean pass=true;
+        String name="ABC";
+        System.out.println("Details:-");
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("Percentage: " +percent);
+        System.out.println("Grade Scored: " +grade);
+        System.out.println("Passed the exams?: " +pass);
+    }
+}
