93a0a803bf059ac7b690a0e20b3b35476de2036e
Nkj-718 <jhanitin16@gmail.com>
2025-04-18 22:48:33 +0530

    Array Input

---
+++ ArrayInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println();
+        for (int i=0; i<n; i++) {
+            System.out.print("Enter element " + i + ": ");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array:-");
+        for (int i=0;i<arr.length;i++) {
+            System.out.println(arr[i] + "\t");
+        }
+    }
+}
