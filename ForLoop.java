b3dd58e2e8696c2feae7fc291122ab9e8848a2d6
Nkj-718 <jhanitin16@gmail.com>
2025-04-18 22:37:19 +0530

    for-loop and basic array implementation

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int[] arr = {1,5,36,74,36,35,54}; //Array Declaration and assigning elements in indices of the array.
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + "\t");
+        }
+    }
+}
